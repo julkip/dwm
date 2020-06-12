a8cd77905ea014a742110bd46ce9acd9e0e8112a
Julian Kippels <kippels@hhu.de>
2020-06-12 20:10:14 +0200

    Additional font

--- config.h
+++ config.h
@@ -12,7 +12,8 @@ static const int showsystray        = 1;     /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const int focusonwheel       = 0;
-static const char *fonts[]          = { "monospace:size=10" };
+static const char *fonts[]          = { "monospace:size=10",
+					"JoyPixels:size=10:antialias=true:autohint=true"};
 static const char dmenufont[]       = "monospace:size=10";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
