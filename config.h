451f16bb44c3ac4fff10d29a5fd1f4434a88995f
Julian Kippels <kippels@hhu.de>
2020-08-23 16:05:40 +0200

    remove swallowing-artifacts from config.h

--- config.h
+++ config.h
@@ -6,7 +6,6 @@
 /* appearance */
 static const unsigned int borderpx  = 3;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
-static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
 static const int rmaster            = 1;        /* 1 means master-area is initially on the right */
 static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
 static const unsigned int systrayspacing = 2;   /* systray spacing */
