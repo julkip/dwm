4358b9f09404e486e692032785df3d6fc3bea4c4
Julian Kippels <julkip@neunerdhausen.de>
2021-03-23 21:48:46 +0100

    mod+numbers seems better than mod+f-keys

--- config.h
+++ config.h
@@ -116,15 +116,15 @@ static Key keys[] = {
 	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
-	TAGKEYS(                        XK_F1,                     0)
-	TAGKEYS(                        XK_F2,                     1)
-	TAGKEYS(                        XK_F3,                     2)
-	TAGKEYS(                        XK_F4,                     3)
-	TAGKEYS(                        XK_F5,                     4)
-	TAGKEYS(                        XK_F6,                     5)
-	TAGKEYS(                        XK_F7,                     6)
-	TAGKEYS(                        XK_F8,                     7)
-//	TAGKEYS(                        XK_9,                      8)
+	TAGKEYS(                        XK_1,                     0)
+	TAGKEYS(                        XK_2,                     1)
+	TAGKEYS(                        XK_3,                     2)
+	TAGKEYS(                        XK_4,                     3)
+	TAGKEYS(                        XK_5,                     4)
+	TAGKEYS(                        XK_6,                     5)
+	TAGKEYS(                        XK_7,                     6)
+	TAGKEYS(                        XK_8,                     7)
+	TAGKEYS(                        XK_9,                     8)
 	{ MODKEY|ShiftMask,             XK_x,      quit,           {0} },
 	{ MODKEY|ControlMask|ShiftMask, XK_x,      quit,           {1} },
 };
